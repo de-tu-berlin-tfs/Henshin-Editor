65703428d97eadc981b825ccba95c4af52093ec3
DanielBinanzer <tri_boon@gmx.de>
2012-11-07 15:34:45 +0100

    Graph- und EdgeLayouts werden nun beim Löschen von Graphen und Regeln ebenfalls gelöscht.

--- TGGEditor/src/tggeditor/commands/delete/rule/DeleteRuleCommand.java
+++ TGGEditor/src/tggeditor/commands/delete/rule/DeleteRuleCommand.java
@@ -4,7 +4,10 @@ import org.eclipse.emf.henshin.model.Node;
 import org.eclipse.emf.henshin.model.Rule;
 import org.eclipse.gef.commands.CompoundCommand;
 
+import tgg.GraphLayout;
+import tggeditor.commands.delete.DeleteGraphCommand;
 import tggeditor.commands.delete.DeleteNodeCommand;
+import tggeditor.util.GraphUtil;
 
 import de.tub.tfs.muvitor.commands.SimpleDeleteEObjectCommand;
 
@@ -24,6 +27,10 @@ public class DeleteRuleCommand extends CompoundCommand {
 		for (Node n : rule.getRhs().getNodes()) {
 			add(new DeleteNodeCommand(n));
 		}
+		GraphLayout divSC = GraphUtil.getGraphLayout(rule.getRhs(), true);
+		GraphLayout divCT = GraphUtil.getGraphLayout(rule.getRhs(), false);
+		if (divSC != null) add (new SimpleDeleteEObjectCommand(divSC));
+		if (divCT != null) add (new SimpleDeleteEObjectCommand(divCT));
 		add(new SimpleDeleteEObjectCommand(rule));
 	}
 
