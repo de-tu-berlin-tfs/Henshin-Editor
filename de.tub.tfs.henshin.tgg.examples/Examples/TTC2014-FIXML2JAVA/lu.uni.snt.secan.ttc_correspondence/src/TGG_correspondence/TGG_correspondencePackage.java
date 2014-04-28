b161763afdec90125c5c25cb58495b709ba865ce
frank.hermann <frank.hermann@uni.lu>
2014-04-28 14:31:57 +0200

    example for TTC 2014, FIXML2Java

---
+++ de.tub.tfs.henshin.tgg.examples/Examples/TTC2014-FIXML2JAVA/lu.uni.snt.secan.ttc_correspondence/src/TGG_correspondence/TGG_correspondencePackage.java
@@ -0,0 +1,160 @@
+/**
+ */
+package TGG_correspondence;
+
+import lu.uni.snt.secan.ttc_xml.tTC_XML.TTC_XMLPackage;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Package</b> for the model.
+ * It contains accessors for the meta objects to represent
+ * <ul>
+ *   <li>each class,</li>
+ *   <li>each feature of each class,</li>
+ *   <li>each enum,</li>
+ *   <li>and each data type</li>
+ * </ul>
+ * <!-- end-user-doc -->
+ * @see TGG_correspondence.TGG_correspondenceFactory
+ * @model kind="package"
+ * @generated
+ */
+public interface TGG_correspondencePackage extends EPackage {
+	/**
+	 * The package name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNAME = "TGG_correspondence";
+
+	/**
+	 * The package namespace URI.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNS_URI = "http://snt.uni.lu/TGGcorrespondence";
+
+	/**
+	 * The package namespace name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNS_PREFIX = "TGGcorrespondence";
+
+	/**
+	 * The singleton instance of the package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	TGG_correspondencePackage eINSTANCE = TGG_correspondence.impl.TGG_correspondencePackageImpl.init();
+
+	/**
+	 * The meta object id for the '{@link TGG_correspondence.impl.CORRImpl <em>CORR</em>}' class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see TGG_correspondence.impl.CORRImpl
+	 * @see TGG_correspondence.impl.TGG_correspondencePackageImpl#getCORR()
+	 * @generated
+	 */
+	int CORR = 0;
+
+	/**
+	 * The feature id for the '<em><b>Src</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int CORR__SRC = TTC_XMLPackage.ABSTRACT_CORR__SRC;
+
+	/**
+	 * The feature id for the '<em><b>Tgt</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int CORR__TGT = TTC_XMLPackage.ABSTRACT_CORR_FEATURE_COUNT + 0;
+
+	/**
+	 * The number of structural features of the '<em>CORR</em>' class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int CORR_FEATURE_COUNT = TTC_XMLPackage.ABSTRACT_CORR_FEATURE_COUNT + 1;
+
+
+	/**
+	 * Returns the meta object for class '{@link TGG_correspondence.CORR <em>CORR</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for class '<em>CORR</em>'.
+	 * @see TGG_correspondence.CORR
+	 * @generated
+	 */
+	EClass getCORR();
+
+	/**
+	 * Returns the meta object for the reference '{@link TGG_correspondence.CORR#getTgt <em>Tgt</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for the reference '<em>Tgt</em>'.
+	 * @see TGG_correspondence.CORR#getTgt()
+	 * @see #getCORR()
+	 * @generated
+	 */
+	EReference getCORR_Tgt();
+
+	/**
+	 * Returns the factory that creates the instances of the model.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the factory that creates the instances of the model.
+	 * @generated
+	 */
+	TGG_correspondenceFactory getTGG_correspondenceFactory();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * Defines literals for the meta objects that represent
+	 * <ul>
+	 *   <li>each class,</li>
+	 *   <li>each feature of each class,</li>
+	 *   <li>each enum,</li>
+	 *   <li>and each data type</li>
+	 * </ul>
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	interface Literals {
+		/**
+		 * The meta object literal for the '{@link TGG_correspondence.impl.CORRImpl <em>CORR</em>}' class.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see TGG_correspondence.impl.CORRImpl
+		 * @see TGG_correspondence.impl.TGG_correspondencePackageImpl#getCORR()
+		 * @generated
+		 */
+		EClass CORR = eINSTANCE.getCORR();
+
+		/**
+		 * The meta object literal for the '<em><b>Tgt</b></em>' reference feature.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		EReference CORR__TGT = eINSTANCE.getCORR_Tgt();
+
+	}
+
+} //TGG_correspondencePackage
---
+++ de.tub.tfs.henshin.tgg.examples/Examples/TTC2014-FIXML2JAVA/lu.uni.snt.secan.ttc_correspondence/src/TGG_correspondence/impl/CORRImpl.java
@@ -0,0 +1,155 @@
+/**
+ */
+package TGG_correspondence.impl;
+
+import TGG_correspondence.CORR;
+import TGG_correspondence.TGG_correspondencePackage;
+import lu.uni.snt.secan.ttc_java.tTC_Java.AbstractTarget;
+import lu.uni.snt.secan.ttc_xml.tTC_XML.impl.AbstractCorrImpl;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>CORR</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link TGG_correspondence.impl.CORRImpl#getTgt <em>Tgt</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class CORRImpl extends AbstractCorrImpl implements CORR {
+	/**
+	 * The cached value of the '{@link #getTgt() <em>Tgt</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTgt()
+	 * @generated
+	 * @ordered
+	 */
+	protected AbstractTarget tgt;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CORRImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return TGG_correspondencePackage.Literals.CORR;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AbstractTarget getTgt() {
+		if (tgt != null && tgt.eIsProxy()) {
+			InternalEObject oldTgt = (InternalEObject)tgt;
+			tgt = (AbstractTarget)eResolveProxy(oldTgt);
+			if (tgt != oldTgt) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, TGG_correspondencePackage.CORR__TGT, oldTgt, tgt));
+			}
+		}
+		return tgt;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AbstractTarget basicGetTgt() {
+		return tgt;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setTgt(AbstractTarget newTgt) {
+		AbstractTarget oldTgt = tgt;
+		tgt = newTgt;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, TGG_correspondencePackage.CORR__TGT, oldTgt, tgt));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case TGG_correspondencePackage.CORR__TGT:
+				if (resolve) return getTgt();
+				return basicGetTgt();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case TGG_correspondencePackage.CORR__TGT:
+				setTgt((AbstractTarget)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case TGG_correspondencePackage.CORR__TGT:
+				setTgt((AbstractTarget)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case TGG_correspondencePackage.CORR__TGT:
+				return tgt != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //CORRImpl
