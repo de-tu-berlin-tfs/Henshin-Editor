5b210e12498b56724ae3f2aaad77d25b66215cdb
Frank HERMANN <frank.hermann@uni.lu>
2015-01-13 17:19:30 +0100

    Completed java file comments as agreed with developers and partners

--- de.tub.tfs.henshin.tgg.examples/Examples/Repeat2While/lu.uni.snt.repeat/src-gen/lu/uni/snt/repeat/parser/antlr/internal/InternalREPEATLexer.java
+++ de.tub.tfs.henshin.tgg.examples/Examples/Repeat2While/lu.uni.snt.repeat/src-gen/lu/uni/snt/repeat/parser/antlr/internal/InternalREPEATLexer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 Henshin developers. All rights reserved. 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     TU Berlin, University of Luxembourg, SES S.A.
+ *******************************************************************************/
 package lu.uni.snt.repeat.parser.antlr.internal;
 
 // Hack: Use our own Lexer superclass by means of import. 
