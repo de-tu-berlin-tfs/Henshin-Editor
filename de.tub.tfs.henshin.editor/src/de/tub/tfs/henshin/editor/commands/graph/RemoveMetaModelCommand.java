c9aae4456ef88ecac414c0295be50304c98d13b8
Frank HERMANN <frank.hermann@uni.lu>
2015-01-19 15:45:21 +0100

    updated comments for java files

--- de.tub.tfs.henshin.editor/src/de/tub/tfs/henshin/editor/commands/graph/RemoveMetaModelCommand.java
+++ de.tub.tfs.henshin.editor/src/de/tub/tfs/henshin/editor/commands/graph/RemoveMetaModelCommand.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 Henshin developers. All rights reserved. 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     TU Berlin, University of Luxembourg, SES S.A.
+ *******************************************************************************/
 /**
  * RemoveMetaModelCommand.java
  * created on 12.02.2012 01:51:39
